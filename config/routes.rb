a9b789c67f44e0795fddbe712ee9f861e86ef839
zhangyanan <ivyzhang_happy@163.com>
2014-07-04 17:03:43 +0800

    配置route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 JCDemo::Application.routes.draw do
+  resources :users
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+  root :to => "home#index"
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
@@ -54,5 +58,5 @@ JCDemo::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+  match ':controller(/:action(/:id))(.:format)'
 end
