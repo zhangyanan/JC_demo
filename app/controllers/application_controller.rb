059aef899eb87b827bac8d1c71bb3d7f60c12830
zhangyanan <ivyzhang_happy@163.com>
2014-07-04 17:03:32 +0800

    登陆和退出

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def current_user
+    User.find session[:user_id] rescue nil
+  end
+  def set_session_user user
+    session[:user_id] = user.id
+    session[:p] = []
+  end
 end
---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,35 @@
+class HomeController < ApplicationController
+
+  def index
+    if current_user
+      home
+    else
+      render :action => :home ,:layout => 'login'
+    end
+  end
+
+  def logout
+    reset_session
+    redirect_to :action => :login
+  end
+
+  def login
+    unless params[:name].blank? || params[:password].blank?
+      if (user = User.find_by_name_and_password(params[:name],params[:password]))
+        set_session_user user
+      else
+        flash[:name] = params[:name]
+        flash[:u_error] = true
+      end
+    end
+    redirect_to :action => :index
+  end
+
+  def home
+    render :layout => "main"
+  end
+
+  private :home
+
+
+end
