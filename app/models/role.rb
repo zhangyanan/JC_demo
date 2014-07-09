cc7129c30c5ff26199e9a45d75832b89a36b5edc
zhangyanan <ivyzhang_happy@163.com>
2014-07-09 16:17:39 +0800

    设置role的状态

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,4 +1,21 @@
 class Role < ActiveRecord::Base
   attr_accessible :description, :id, :name
   has_many :user
+
+#  role 状态:启用 禁用
+  ENABLED = 1
+  DISABLED = 0
+
+  def enabled?
+    self.state == ENABLED
+  end
+
+  def disabled?
+    self.state == DISABLED
+  end
+
+  def used?
+    !Role.find_by_user_id(user_id).empty?
+  end
+
 end
