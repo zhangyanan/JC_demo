3f2801521710528c2aabf8ef98b6a39dcd3d122c
zhangyanan <ivyzhang_happy@163.com>
2014-09-12 12:16:37 +0800

    state是字符串类型，转为int进行判断

--- app/models/common_active_record.rb
+++ app/models/common_active_record.rb
@@ -5,11 +5,19 @@ module CommonActiveRecord
   DISABLED = 0
 
   def state_text
-    case self.state
+    case self.state.to_i
       when ENABLED
         '可用'
       when DISABLED
         '禁用'
     end
+  end
+
+  def enabled?
+    self.state == ENABLED
+  end
+
+  def disabled?
+    self.state == DISABLED
   end
 end
