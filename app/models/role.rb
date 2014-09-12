0923928732944dd414e9c4d0ec7ae5ef20efb410
zhangyanan <ivyzhang_happy@163.com>
2014-09-12 11:03:33 +0800

    建立role与rights的关联，包含通用状态方法

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,8 +1,9 @@
 class Role < ActiveRecord::Base
   attr_accessible :description, :id, :name
-
+  include CommonActiveRecord
   has_many :role_users
   has_many :users, :through => :role_users
+  has_many :rights, :through => :role_rights
 
 #  role 状态:启用 禁用
   ENABLED = 1
