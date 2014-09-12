2f37532d9121f9da79c2052dbe5db60e15688dd3
zhangyanan <ivyzhang_happy@163.com>
2014-09-12 10:26:48 +0800

    通用的model状态修改

---
+++ app/helpers/simple_enable_disable.rb
@@ -0,0 +1,21 @@
+#encoding:utf-8
+module SimpleEnableDisable
+
+  def simple_modify_state model,state
+    params_name = model.model_name.underscore
+    id = (params[params_name]) && params[params_name][:id] || params[:id]
+    record = model find id
+    if record
+      transaction do
+        if record.state != state
+          record.update_attributes! state: state
+        end
+      end
+    end
+  end
+
+  def simple_enable model,success_action = nil
+    simple_modify_state model,model::ENABLED
+    redirect_to :action => success_action || :index
+  end
+end
