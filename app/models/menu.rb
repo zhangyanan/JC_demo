39ae4fff4cd8347d16b0ed221a0a393e00298e45
zhangyanan <ivyzhang_happy@163.com>
2014-09-12 11:02:29 +0800

    menu model 建立与其他model的关联

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,11 +1,27 @@
 class Menu < ActiveRecord::Base
-  attr_accessible :id, :name, :parent_menu_id, :url,:right_id
+  attr_accessible :id, :name, :parent_menu_id, :url, :right_id
+  include CommonActiveRecord
+  belongs_to :right
+  belongs_to :parent,:class_name => "Menu",:foreign_key => "parent_menu_id"
 
   def self.root
     Menu.find 1
   end
 
+  def self.root_menu
+    where(parent_menu_id: nil).order("id").all
+  end
+
   def children
     Menu.all :conditions => ['parent_menu_id = ?', self.id]
   end
+
+  def menu_right
+    self.right.name
+  end
+
+  def parent_menu
+
+  end
+
 end
