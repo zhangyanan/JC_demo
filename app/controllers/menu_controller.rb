830f755aabec28cf96a774eab8ac8b768d014e75
zhangyanan <ivyzhang_happy@163.com>
2014-09-12 11:05:17 +0800

    菜单controller

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -1,2 +1,31 @@
+#encoding:utf-8
 class MenuController < ApplicationController
+  def index
+    @title = "当前位置 菜单列表"
+    @menus = set_paginate Menu.order("id ASC")
+  end
+
+  def edit
+    @menu = Menu.find(params[:id])
+    @parent_menus = Menu.root_menu
+    @rights = Right.all_names
+  end
+
+  def update
+    @menu = Menu.find params[:id]
+    if @menu.update_attributes(params[:menu])
+      redirect_to :index
+    else
+      flash[:message] = "无法保存"
+      redirect_to :action => :edit, :id => params[:id]
+    end
+  end
+
+  def enable
+    simple_enable Menu
+  end
+
+  def disable
+    simple_disable Menu
+  end
 end
