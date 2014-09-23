dd519932550986eb1c2f3413bf8c7f5365e48d93
zhangyanan <ivyzhang_happy@163.com>
2014-09-23 15:39:14 +0800

    project的名称和创建者写成helper方便处理

--- app/helpers/project_helper.rb
+++ app/helpers/project_helper.rb
@@ -1,2 +1,7 @@
 module ProjectHelper
+  def project_and_creator project
+    content_tag(:div,:class=> 'project_and_creator') do
+      project.project_creator.name+ "/"+ project.name
+    end
+  end
 end
