4c18a70cb23b883c25271de058773632031a18d7
zhangyanan <ivyzhang_happy@163.com>
2014-09-24 14:53:48 +0800

    schema：给projects表增加项目路径

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140922074053) do
+ActiveRecord::Schema.define(:version => 20140923075038) do
 
   create_table "card_records", :id => false, :force => true do |t|
     t.integer "id",                         :null => false
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(:version => 20140922074053) do
     t.datetime "created_at",                 :null => false
     t.datetime "updated_at",                 :null => false
     t.integer  "tasks_count", :default => 0
+    t.string   "project_url"
   end
 
   create_table "rights", :force => true do |t|
