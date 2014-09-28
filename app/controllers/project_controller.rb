92f9b9b30c3d4f50ca5993879784e6b2e14298fe
zhangyanan <ivyzhang_happy@163.com>
2014-09-28 13:37:16 +0800

    project的controller新增create方法和delete方法

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -5,77 +5,46 @@ class ProjectController < ApplicationController
     @projects = Project.includes(:user)
   end
 
-  # GET /projects/1
-  # GET /projects/1.json
-  def show
 
-  end
-
-  # GET /projects/new
-  # GET /projects/new.json
   def new
+    @title = "当前位置 新建项目"
     @project = Project.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @project }
-    end
   end
 
-  # GET /projects/1/edit
   def edit
+    @title = "当前位置 编辑项目"
     @project = Project.find(params[:id])
   end
 
-  # POST /projects
-  # POST /projects.json
+
   def create
     @project = Project.new(params[:project])
-
-    respond_to do |format|
-      if @project.save
-        format.html { redirect_to @project, notice: 'Project was successfully created.' }
-        format.json { render json: @project, status: :created, location: @project }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
-    end
+    @project.creator = session[:user_id]
+    @project.state = CommonActiveRecord::ENABLED
+    @project.save
+    redirect_to :action => :index
   end
 
-  # PUT /projects/1
-  # PUT /projects/1.json
+
   def update
     @project = Project.find(params[:id])
-
-    respond_to do |format|
-      if @project.update_attributes(params[:project])
-        format.html { redirect_to @project, notice: 'Project was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
-  # DELETE /projects/1
-  # DELETE /projects/1.json
-  def destroy
-    @project = Project.find(params[:id])
-    @project.destroy
 
-    respond_to do |format|
-      format.html { redirect_to projects_url }
-      format.json { head :no_content }
+  def delete
+    @project = Project.find(params[:id])
+    Project.transaction do
+      @project.destroy
     end
+    redirect_to :action => :index
   end
 
   def query
     @title = "当前位置 项目搜索"
-    projects1 = Project.where("name like ?","%#{params[:name].downcase}%")
-    projects2 =  Project.where("name like ?","%#{params[:name].upcase}%")
+    projects1 = Project.where("name like ?", "%#{params[:name].downcase}%")
+    projects2 = Project.where("name like ?", "%#{params[:name].upcase}%")
     @projects = projects1 + projects2
     render :action => :index
   end
+
 end
