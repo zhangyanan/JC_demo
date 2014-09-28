506b466adb73d8c508161dc493ebcd56baa73892
zhangyanan <ivyzhang_happy@163.com>
2014-09-28 10:50:51 +0800

    增加project的属性访问器

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,5 @@ class Project < ActiveRecord::Base
   has_many :tasks
   belongs_to :project_creator,:class_name => 'User',:foreign_key => 'creator'
   belongs_to :user
-  attr_accessible :creator, :description, :id, :name, :state,:group_id
+  attr_accessible :creator, :description, :id, :name, :state,:group_id,:project_url
 end
