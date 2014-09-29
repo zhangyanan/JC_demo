70f49f13b1a77b359b5aff081d61ee26ad9a9afd
zhangyanan <ivyzhang_happy@163.com>
2014-09-29 16:40:44 +0800

    员工性别1 代表男 0代表女

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,6 +1,6 @@
 class Employee < ActiveRecord::Base
   attr_accessible :birthday, :code, :department_id, :expired_at, :id, :name, :remark, :sex, :state
 
-  MALE = 1
-  FEMALE = 0
+  MALE = 1 #男
+  FEMALE = 0 #女
 end
