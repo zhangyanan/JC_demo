b17ac574664de31d1baa625ef68f597151af184f
zhangyanan <ivyzhang_happy@163.com>
2014-09-30 12:20:53 +0800

    编辑资料

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,5 +1,6 @@
 #encoding:utf-8
 class EmployeesController < ApplicationController
+  STATES = [["在职",Employee::ONJOIB],["离职",Employee::OFFJOIB,2],["出差",Employee::AWAYJOIB,3],["产假",Employee::MATERNITYlEAVE,4],["陪产假",Employee::PATERNITYlEAVE,5]]
   def index
     @title = "当前位置 员工列表"
     @employees = set_paginate Employee.order('name')
@@ -11,13 +12,15 @@ class EmployeesController < ApplicationController
 
 
   def new
-    @title = "当前位置 新增员工"
-    @states = [["在职",Employee::ONJOIB],["离职",Employee::OFFJOIB,2],["出差",Employee::AWAYJOIB,3],["产假",Employee::MATERNITYlEAVE,4],["陪产假",Employee::PATERNITYlEAVE,5]]
+    @title = "当前位置 新增员工资料"
+    @states = STATES
     @employee = Employee.new
   end
 
 
   def edit
+    @title = "当前位置 编辑员工资料"
+    @states = STATES
     @employee = Employee.find(params[:id])
   end
 
@@ -33,6 +36,9 @@ class EmployeesController < ApplicationController
 
   def update
     @employee = Employee.find(params[:id])
+    @employee.update_attributes(params[:employee])
+    @employee.save
+    redirect_to :action => :index
   end
 
 
