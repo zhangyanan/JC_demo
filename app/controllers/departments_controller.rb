de92aaa7e054b313430b9087e62f3713567c98f1
zhangyanan <ivyzhang_happy@163.com>
2014-10-07 18:37:20 +0800

    部门管理的页面

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,83 +1,46 @@
+#encoding:utf-8
 class DepartmentsController < ApplicationController
-  # GET /departments
-  # GET /departments.json
   def index
+    @title = "当前位置 部门列表"
     @departments = Department.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @departments }
-    end
   end
 
-  # GET /departments/1
-  # GET /departments/1.json
   def show
     @department = Department.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @department }
-    end
   end
 
-  # GET /departments/new
-  # GET /departments/new.json
   def new
+    @title = "当前位置 新建部门"
     @department = Department.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @department }
-    end
   end
 
-  # GET /departments/1/edit
   def edit
+    @title = "当前位置 编辑部门"
     @department = Department.find(params[:id])
   end
 
-  # POST /departments
-  # POST /departments.json
   def create
     @department = Department.new(params[:department])
-
-    respond_to do |format|
-      if @department.save
-        format.html { redirect_to @department, notice: 'Department was successfully created.' }
-        format.json { render json: @department, status: :created, location: @department }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @department.errors, status: :unprocessable_entity }
-      end
-    end
+    @department.state = CommonActiveRecord::ENABLED
+    @department.save
+    redirect_to :action => :index
   end
 
-  # PUT /departments/1
-  # PUT /departments/1.json
   def update
     @department = Department.find(params[:id])
-
-    respond_to do |format|
-      if @department.update_attributes(params[:department])
-        format.html { redirect_to @department, notice: 'Department was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @department.errors, status: :unprocessable_entity }
-      end
-    end
+    @department.update_attributes(params[:department])
+    @department.save
+    redirect_to :action => :index
   end
 
-  # DELETE /departments/1
-  # DELETE /departments/1.json
-  def destroy
+  def delete
     @department = Department.find(params[:id])
     @department.destroy
+    redirect_to :action => :index
+  end
 
-    respond_to do |format|
-      format.html { redirect_to departments_url }
-      format.json { head :no_content }
-    end
+  def query
+    @departments = set_paginate Department.where("name like ?","%#{params[:name]}%")
+    render :action => :index
   end
 end
