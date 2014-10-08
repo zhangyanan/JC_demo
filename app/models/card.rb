6df0d822cc2bf527f307cd12daaf965e7c7546ad
zhangyanan <ivyzhang_happy@163.com>
2014-10-08 15:22:30 +0800

    卡片的页面

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,4 +2,6 @@ class Card < ActiveRecord::Base
   attr_accessible :card_number, :created_at, :employee_id, :id, :lost_at, :update_at
 
   belongs_to :employee
+  has_many :card_records
+  has_many :records,:through => :card_records
 end
