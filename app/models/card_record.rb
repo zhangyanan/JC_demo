dd8aafe2900c88d235f1c5cb93da1ad944c34e97
zhangyanan <ivyzhang_happy@163.com>
2014-10-08 15:22:56 +0800

    建立员工卡与考勤记录的关联

--- app/models/card_record.rb
+++ app/models/card_record.rb
@@ -1,2 +1,4 @@
 class CardRecord < ActiveRecord::Base
+  belongs_to :card
+  belongs_to :record
 end
--- app/models/record.rb
+++ app/models/record.rb
@@ -1,3 +1,8 @@
 class Record < ActiveRecord::Base
-  attr_accessible :id, :in_out, :terminal_id
+  attr_accessible :id, :in_out, :terminal_ip
+
+  include CommonActiveRecord
+
+  has_many :card_records
+  has_many :cards,:through => :card_records
 end
